1953faaa0cb7061b5ba3ad62dabe70374e671559
subbni <slfkak@naver.com>
2025-06-09 17:54:56 +0900

    [feat] Swagger 설정 추가 (#1) - properties를 가독성이 좋은 yml로 변경 - active.profiles 설정 추가 - swagger 설정 추가

---
+++ src/main/java/com/example/assignment/global/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.assignment.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("바로인턴13기 백엔드 Java 과제 API Document")
+                .version("v0.0.1")
+                .description("바로인턴13기 백엔드 Java 과제에 대한 API 명세서입니다.");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
