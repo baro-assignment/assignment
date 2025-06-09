7fb0acd86d658bcc7d65af7064a3feefde131bb3
subbni <slfkak@naver.com>
2025-06-09 18:56:01 +0900

    [feat] CustomException 및 ExceptionType 구현 (#2)

---
+++ src/main/java/com/example/assignment/global/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.example.assignment.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ExceptionType exceptionType;
+
+    public CustomException(ExceptionType exceptionType) {
+        super(exceptionType.getMessage());
+        this.exceptionType = exceptionType;
+    }
+
+    public CustomException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptionType = exceptionType;
+    }
+}
