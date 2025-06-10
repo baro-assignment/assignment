c6887bc0d7d2a31358c0c04ef439cccccdf289b5
subbni <slfkak@naver.com>
2025-06-11 01:06:04 +0900

    [feat] Swagger OperationCustomizer 구현 및 등록 (#15) - ApiErrorResponses와 ApiErrorResponse 어노테이션을 사용할 수 있도록 OperationCustomizer를 구현 - 구현한 OperationCustomizer를 SwaggerConfig에서 등록

--- src/main/java/com/example/assignment/global/config/SwaggerConfig.java
+++ src/main/java/com/example/assignment/global/config/SwaggerConfig.java
@@ -1,13 +1,19 @@
 package com.example.assignment.global.config;
 
+import com.example.assignment.global.customizer.ApiErrorResponseCustomizer;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final ApiErrorResponseCustomizer apiErrorResponseCustomizer;
 
     @Bean
     public OpenAPI openAPI() {
@@ -19,5 +25,13 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components())
                 .info(info);
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("assignment API")
+                .addOperationCustomizer(apiErrorResponseCustomizer)
+                .build();
     }
 }
