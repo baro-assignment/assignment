91287ba5cabeef68209813aae9449963e01e14e1
subbni <slfkak@naver.com>
2025-06-11 01:07:19 +0900

    [feat] ApiErrorResponses 어노테이션 적용 (#15)

--- src/main/java/com/example/assignment/domain/auth/controller/AuthControllerSpecification.java
+++ src/main/java/com/example/assignment/domain/auth/controller/AuthControllerSpecification.java
@@ -4,6 +4,8 @@ import com.example.assignment.domain.auth.dto.request.LoginRequest;
 import com.example.assignment.domain.auth.dto.request.SignUpRequest;
 import com.example.assignment.domain.auth.dto.response.LoginResponse;
 import com.example.assignment.domain.auth.dto.response.SignUpResponse;
+import com.example.assignment.global.annotation.ApiErrorResponses;
+import com.example.assignment.global.exception.ExceptionType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +15,11 @@ public interface AuthControllerSpecification {
 
     @Operation(summary = "회원가입",
     description = "사용자 정보를 받아 회원가입을 수행합니다.")
+    @ApiErrorResponses(value = {ExceptionType.USER_ALREADY_EXISTS})
     ResponseEntity<SignUpResponse> signUp(SignUpRequest signUpRequest);
 
     @Operation(summary = "로그인",
     description = "사용자 인증 정보를 받아 로그인을 수행합니다.")
+    @ApiErrorResponses(value = {ExceptionType.INVALID_CREDENTIALS})
     ResponseEntity<LoginResponse> login(LoginRequest loginRequest);
 }
--- src/main/java/com/example/assignment/global/dto/response/ErrorResponse.java
+++ src/main/java/com/example/assignment/global/dto/response/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.example.assignment.global.dto.response;
 
 import com.example.assignment.global.exception.ExceptionType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
@@ -22,9 +23,12 @@ public class ErrorResponse {
     }
 
     @Getter
+    @Schema(description = "에러 정보")
     public static class ErrorDetail {
 
+        @Schema(description = "에러 코드")
         private String code;
+        @Schema(description = "에러 메시지")
         private String message;
 
         public ErrorDetail(String code, String message) {
