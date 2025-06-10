98fe448d8197ce97a7cc66fb0fe3aba57b4cb093
subbni <slfkak@naver.com>
2025-06-11 01:07:19 +0900

    [feat] ApiErrorResponses 어노테이션 적용 (#15)

--- src/main/java/com/example/assignment/domain/admin/controller/AdminControllerSpecification.java
+++ src/main/java/com/example/assignment/domain/admin/controller/AdminControllerSpecification.java
@@ -1,6 +1,8 @@
 package com.example.assignment.domain.admin.controller;
 
 import com.example.assignment.domain.admin.dto.response.GrantAdminRoleResponse;
+import com.example.assignment.global.annotation.ApiErrorResponses;
+import com.example.assignment.global.exception.ExceptionType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ public interface AdminControllerSpecification {
 
     @Operation(summary = "관리자 권한 부여",
     description = "사용자 id를 받아 해당 사용자에게 관리자 권한을 부여합니다.")
+    @ApiErrorResponses(value = {ExceptionType.AUTHENTICATION_REQUIRED, ExceptionType.ACCESS_DENIED, ExceptionType.USER_NOT_FOUND})
     ResponseEntity<GrantAdminRoleResponse> grantAdminRole(Long userId);
 
 }
