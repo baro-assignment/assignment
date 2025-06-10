58d55667e94141371f0b3945bf6438ce8112fc83
subbni <slfkak@naver.com>
2025-06-11 01:27:41 +0900

    [feat] Swagger 사용시 JWT Authorization Header를 설정할 수 있도록 추가 (#15)

--- src/main/java/com/example/assignment/global/config/SwaggerConfig.java
+++ src/main/java/com/example/assignment/global/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import com.example.assignment.global.customizer.ApiErrorResponseCustomizer;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +24,20 @@ public class SwaggerConfig {
                 .version("v0.0.1")
                 .description("바로인턴13기 백엔드 Java 과제에 대한 API 명세서입니다.");
 
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .description("* Bearer prefix를 제외한 토큰을 입력하세요.");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Token");
+
         return new OpenAPI()
                 .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("Token", securityScheme);
     }
 
     @Bean
